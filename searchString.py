9a6d4e35ed62b78e2d1723be5994d199612aaf77
r-fine <saiadul.arfain@tuta.io>
2022-04-09 04:27:21 +0600

    q2

--- searchString.py
+++ searchString.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 total_alph = 26
 dct = {
     'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25
@@ -18,8 +16,6 @@ def computeTransFun(pattern, m, transFun):
             transFun[i][dct[pattern[i]]] = i + 1  
             longest = transFun[longest][dct[pattern[i]]]
 
-    # print(pd.DataFrame(transFun))
-
 def search(pattern, text, t):
     m = len(pattern)
     n = len(text)
@@ -31,7 +27,6 @@ def search(pattern, text, t):
         j = transFun[j][dct[text[i]]]
         if (j == m):
             c+=1
-            # print("pattern found at index", i - m + 1)
     
     print(f"pattern found for T{t} {c} times")
 
