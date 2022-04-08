2bf79516df2b1c8ccddd6082ffb585f2bc0b4a0c
r-fine <saiadul.arfain@tuta.io>
2022-04-09 04:27:07 +0600

    q1

---
+++ transitionTable.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+total_alph = 26
+dct = {
+    'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25
+}
+
+def computeTransFun(pattern, m, transFun):
+
+    longest = 0 
+    for x in range(total_alph):
+    	transFun[0][x] = 0
+    transFun[0][dct[pattern[0]]] = 1  
+    for i in range(1, m+1): 
+    	for x in range(total_alph):
+    		transFun[i][x] = transFun[longest][x]   
+    	if (i < m):
+            transFun[i][dct[pattern[i]]] = i + 1  
+            longest = transFun[longest][dct[pattern[i]]]
+
+    print(pd.DataFrame(transFun))
+
+def search(pattern, text):
+    m = len(pattern)
+    n = len(text)
+    transFun = [[0 for i in range(total_alph)] for j in range(m + 1)]
+    computeTransFun(pattern, m, transFun) 
+
+text = "abababcabababcabababc"
+pattern = "ababa"
+
+search(pattern, text)
